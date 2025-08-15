03db11bd2ddccaf39abd3557500fcc329e05e91b
M ALI RAZA <casim9530@gmail.com>
2025-08-15 06:35:08 -0700

    variable.js
    variable

---
+++ variable.js
@@ -0,0 +1,31 @@
+console.log("hello world");
+let a = 5;
+console.log(a);
+console.log("hello ali");
+
+let data = {
+  name: "ali",
+  uni: "iqra uni",
+  campus: 5,
+  student: true,
+  friend: "",
+};
+console.log(data);
+
+// data is a variable and name,uni etc its values.let is a
+// let’s break this down in simple terms — because let, const, and var are basically ways to create variables in JavaScript,
+// but they each behave differently.
+
+//avaScript variable naming rules ✅
+
+// Must start with a letter (a–z, A–Z), underscore (_), or dollar sign ($).
+
+// Can contain letters, numbers, underscores, or dollar signs after the first character.
+
+// Cannot start with a number.
+
+// Cannot use spaces or most special characters (@ # % - ! & etc.).
+
+// Case-sensitive (name ≠ Name).
+
+// Cannot use reserved keywords (let, if, class, etc.).
