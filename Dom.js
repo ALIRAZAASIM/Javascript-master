851414bfe111b42bd30622985f6f08658e264818
M ALI RAZA <casim9530@gmail.com>
2025-08-25 07:30:37 -0700

    dom

--- Dom.js
+++ Dom.js
@@ -3,3 +3,9 @@ console.log(ele);
 
 let ele2 = document.querySelector(".container");
 console.log(ele2.innerHTML);
+
+ele.innerText = "aliraza";
+ele.textContent = "again change it";
+
+let anc = document.querySelector("a");
+anc.setAttribute("href", "https://www.google.com/");
