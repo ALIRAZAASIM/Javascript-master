3edbf09e15723523b491479db4cd383d6d5f66ba
M ALI RAZA <casim9530@gmail.com>
2025-08-26 14:13:52 -0700

    dom

--- Dom.js
+++ Dom.js
@@ -9,3 +9,23 @@ ele.textContent = "again change it";
 
 let ancher = document.querySelector("a");
 ancher.setAttribute("href", "https://www.google.com/");
+
+// for styling
+
+ele.style.background = "blue";
+ele.style.padding = "10px";
+
+// remove class
+// ele2.classList.remove("container");
+
+// add class
+ele.classList.add("container2");
+
+let btn = document.querySelector("button");
+let img = document.querySelector("img"); // should be "img" not "image"
+
+function changeImgSize() {
+  img.style.height = "200px";
+  img.style.width = "200px";
+}
+btn.addEventListener("click", changeImgSize);
